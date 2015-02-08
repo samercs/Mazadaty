89598aad39a9e6b7bc0e562ced1c2a14a322f542
Andy Mehalick <mehalick@gmail.com>
2015-02-08 16:46:24 +0000

    MZ-1 added Models project

---
+++ src/Mzayad.Models/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Mzayad.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+      
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedUtc { get; set; }
+
+        //public string CreatedLocalTime
+        //{
+        //    get { return DateTimeFormatter.ToLocalTime(CreatedUtc); }
+        //}
+
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        //{
+        //    return await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        //}
+    }
+}
---
+++ src/Mzayad.Models/ModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mzayad.Models
+{
+    [Serializable]
+    public abstract class ModelBase
+    {
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime CreatedUtc { get; set; }
+    }
+}
