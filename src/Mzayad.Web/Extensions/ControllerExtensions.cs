367807dfa340cc6f54187efeb773a8ada5acd6ef
Andy Mehalick <mehalick@gmail.com>
2015-02-09 19:39:30 +0000

    MZ-4 added EmailTemplate

---
+++ src/Mzayad.Web/Extensions/ControllerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Mzayad.Web.Controllers;
+using OrangeJetpack.Base.Web;
+using OrangeJetpack.Services.Models;
+
+namespace Mzayad.Web.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static Email WithTemplate(this Email email, ApplicationController controller)
+        {
+            var messageWithTemplate = ControllerUtilities.GetPartialViewAsString(controller, "EmailTemplate", email);
+            email.Message = messageWithTemplate;
+
+            return email;
+        }
+    }
+}
