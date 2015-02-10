99a839856be7c8a261908cb21b5ab1333fe7f069
Andy Mehalick <mehalick@gmail.com>
2015-02-10 13:17:09 +0000

    MZ-10 added Localization NuGet packages

---
+++ src/Mzayad.Web/Scripts/site.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+
+    $(function () {
+        bindEvents();
+    });
+
+    var bindEvents = function() {
+        $("form").on("change", ".auto-submit", autoSubmitForm);
+        $(".localized-content").on("input", ".localized-input[data-primary='true']", setHiddenLocalizedContent);   
+    };
+
+    var setHiddenLocalizedContent = function () {
+        var $this = $(this);
+        $this.siblings("input[type='hidden']").val($this.val());
+    };
+
+    var autoSubmitForm = function() {
+        $(this).parents("form").submit();
+    };
+
+})(jQuery);
