a57e8ba9e441ba0446046c3324f6363e01012a26
Andy Mehalick <mehalick@gmail.com>
2015-02-11 11:49:03 +0000

    MZ-2 added Status Message bits

--- src/Mzayad.Web/Extensions/HtmlExtensions.cs
+++ src/Mzayad.Web/Extensions/HtmlExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using OrangeJetpack.Base.Web;
 
 namespace Mzayad.Web.Extensions
 {
@@ -15,5 +16,72 @@ namespace Mzayad.Web.Extensions
         {
             return htmlHelper.LanguageCode().Equals("ar");
         }
+
+        public static MvcHtmlString StatusMessage(this HtmlHelper htmlHelper, StatusMessage statusMessage)
+        {
+            var div = new TagBuilder("div");
+            div.AddCssClass(statusMessage.GetCssClass());
+            div.InnerHtml += GetStatusMessageIcon(statusMessage.StatusMessageType);
+            div.InnerHtml += "<span>" + statusMessage.Message + "</span>";
+            div.InnerHtml += "<div class='clearfix'></div>";
+
+            return MvcHtmlString.Create(div.ToString(TagRenderMode.Normal));
+        }
+
+        public static MvcHtmlString StatusMessage(this HtmlHelper htmlHelper, string message,
+            StatusMessageType statusMessageType = StatusMessageType.Success,
+            StatusMessageFormat statusMessageFormat = StatusMessageFormat.Normal)
+        {
+            var statusMessage = new StatusMessage(message, statusMessageType, statusMessageFormat, false);
+            return StatusMessage(htmlHelper, statusMessage);
+        }
+
+        private static string GetStatusMessageIcon(StatusMessageType statusMessageType)
+        {
+            var icon = new TagBuilder("i");
+            icon.AddCssClass("fa fa-3x pull-left");
+            switch (statusMessageType)
+            {
+                case StatusMessageType.Success:
+                    icon.AddCssClass("fa-check-square-o");
+                    break;
+                case StatusMessageType.Information:
+                    icon.AddCssClass("fa-question-circle");
+                    break;
+                case StatusMessageType.Warning:
+                    icon.AddCssClass("fa-warning");
+                    break;
+                case StatusMessageType.Error:
+                    icon.AddCssClass("fa-exclamation-triangle");
+                    break;
+            }
+            return icon.ToString();
+        }
+
+        public static Css Css(this HtmlHelper htmlHelper)
+        {
+            return new Css(htmlHelper);
+        }
+    }
+
+        public class Css
+    {
+        private readonly HtmlHelper _htmlHelper;
+
+        public Css()
+        {
+            
+        }
+
+        public Css(HtmlHelper htmlHelper)
+        {
+            _htmlHelper = htmlHelper;
+        }
+
+        public string GridTable = "table table-bordered table-hover table-striped";
+        public string LayoutTable = "table";
+        public string FormNarrow = "col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2";
+        public string FormNormal = "col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12";
+        public string FormWide = "col-xs-12";
     }
 }
