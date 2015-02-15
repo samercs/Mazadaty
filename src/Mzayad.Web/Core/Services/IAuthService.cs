6ae80f85a0672b2b98328547631f131f11dfdb55
Andy Mehalick <mehalick@gmail.com>
2015-02-15 12:35:54 +0000

    MZ-15 added IAuthService.UserHostAddress()

--- src/Mzayad.Web/Core/Services/IAuthService.cs
+++ src/Mzayad.Web/Core/Services/IAuthService.cs
@@ -15,6 +15,7 @@ namespace Mzayad.Web.Core.Services
         bool IsLocal();
         string CurrentUserId();
         string AnonymousId();
+        string UserHostAddress();
 
         Task<ApplicationUser> SignIn(string userName, string password, bool rememberMe);
         Task<ApplicationUser> SignIn(ApplicationUser user);
