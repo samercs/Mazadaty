86355cd9f22a2cb870e6c33a28a8cc1fd4a83cae
Andy Mehalick <mehalick@gmail.com>
2015-02-28 15:35:59 +0000

    MZ-22 added Web API to Autofac

--- src/Mzayad.Web/App_Start/Startup.Auth.cs
+++ src/Mzayad.Web/App_Start/Startup.Auth.cs
@@ -3,15 +3,21 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
 using Mzayad.Data;
 using Mzayad.Models;
 using Mzayad.Web.Core.Identity;
+using Mzayad.Web.Core.Providers;
 using Owin;
 
 namespace Mzayad.Web
 {
     public partial class Startup
     {
+        //public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        //public static string PublicClientId { get; private set; }
+        
         public void ConfigureAuth(IAppBuilder app)
         {
             app.CreatePerOwinContext(DataContext.Create);
@@ -31,6 +37,18 @@ namespace Mzayad.Web
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enable the application to use bearer tokens to authenticate users
+            app.UseOAuthBearerTokens(new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new ApplicationOAuthProvider("self"),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                //AllowInsecureHttp = true
             });
         }
     }
