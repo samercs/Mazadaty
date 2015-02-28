97874ef0b1fe036e78a5a8ca13066784133c8d48
Andy Mehalick <mehalick@gmail.com>
2015-02-28 11:01:46 +0000

    MZ-22 added Microsoft.AspNet.WebApi.Owin

--- src/Mzayad.Web/App_Start/WebApiConfig.cs
+++ src/Mzayad.Web/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
 
 namespace Mzayad.Web
 {
@@ -9,6 +10,11 @@ namespace Mzayad.Web
     {
         public static void Register(HttpConfiguration config)
         {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
