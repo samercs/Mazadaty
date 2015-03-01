22a0d76cc02eb0626bfef2184c747181d8c7059d
samercs <samer_mail_2006@yahoo.com>
2015-03-02 00:32:54 +0200

    MZ-24 Fix changeCountry() method in ~/scripts/address.js to capture their input before we change the country and then apply it back afterwards

--- src/Mzayad.Web/Scripts/address.js
+++ src/Mzayad.Web/Scripts/address.js
@@ -1,16 +1,13 @@
-﻿(function ($) {
+﻿$(function () {
 
-    $(function () {
-        $("#Address_CountryCode").on("change", changeCountry);
-    });
+    
+    $("#Address_CountryCode").change(function() {
+        
+        copyData();
 
-    var addressLoading = $("#AddressEntryLoading");
-    var addressForm = $("#AddressEntryAddress");
-    var countryFlag = $("#AddressEntryFlag");
-
-    var changeCountry = function () {
         var $this = $(this);
         var countryCode = $this.val();
+
         if (countryCode.length === 0) {
             return;
         }
@@ -18,7 +15,17 @@
         setCountryFlag(countryCode);
         showLoading();
         addressForm.load("/account/changecountry?countryCode=" + countryCode, hideLoading);
-    };
+
+        
+
+    });
+   
+
+    var addressLoading = $("#AddressEntryLoading");
+    var addressForm = $("#AddressEntryAddress");
+    var countryFlag = $("#AddressEntryFlag");
+
+    
 
     var setCountryFlag = function (countryCode) {
         countryCode = countryCode.toLowerCase();
@@ -34,6 +41,9 @@
         addressLoading.hide();
         addressForm.css("visibility", "visible");
         resetFormValidation();
+
+
+        pastData();
     };
 
     var resetFormValidation = function() {
@@ -45,4 +55,42 @@
         $.validator.unobtrusive.parse("#main form");
     };
 
-})(jQuery);
+    var address = function() {
+
+        this.Line1 = "";
+        this.Line2 = "";
+        this.Line3 = "";
+        this.Line4 = "";
+        this.CityArea = "";
+        this.StateProvince = "";
+        this.PostalCode = "";
+
+    };
+
+    var data = new address();
+
+    var copyData = function() {
+
+        data.Line1 = $("#Address_AddressLine1").val();
+        data.Line2 = $("#Address_AddressLine2").val();
+        data.Line4 = $("Address_AddressLine4").val();
+        data.Line3 = $("Address_AddressLine3").val();
+        data.CityArea = $("#Address_CityArea").val();
+        data.StateProvince = $("#Address_StateProvince").val();
+        data.PostalCode = $("#Address_PostalCode").val();
+        
+    };
+
+    var pastData = function() {
+
+        $("#Address_AddressLine1").val(data.Line1);
+        $("#Address_AddressLine2").val(data.Line2);
+        $("#Address_AddressLine3").val(data.Line3);
+        $("#Address_AddressLine4").val(data.Line4);
+        $("#Address_CityArea").val(data.CityArea);
+        $("#Address_StateProvince").val(data.StateProvince);
+        $("#Address_PostalCode").val(data.PostalCode);
+        
+    };
+
+});
