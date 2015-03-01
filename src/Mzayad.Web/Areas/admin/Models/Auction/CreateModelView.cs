650564f6623bf70211a1103949704e9ba3ea4008
Andy Mehalick <mehalick@gmail.com>
2015-03-01 10:53:10 +0000

    MZ-18 set BuyNowEnabled to false as default

--- src/Mzayad.Web/Areas/admin/Models/Auction/CreateModelView.cs
+++ src/Mzayad.Web/Areas/admin/Models/Auction/CreateModelView.cs
@@ -1,19 +1,16 @@
-﻿using System;
+﻿using Mzayad.Models.Enum;
+using Mzayad.Services;
+using Mzayad.Web.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
-using Mzayad.Models;
-using Mzayad.Models.Enum;
-using Mzayad.Services;
-using Mzayad.Web.Extensions;
 
 namespace Mzayad.Web.Areas.admin.Models.Auction
 {
     public class CreateModelView
     {
-        
         public List<SelectListItem> ProductList { get; set; }
         public Mzayad.Models.Auction Auction { get; set; }
         public List<SelectListItem> StatusList { get; set; } 
@@ -29,7 +26,7 @@ namespace Mzayad.Web.Areas.admin.Models.Auction
                 StartUtc = dateNow,
                 Status = AuctionStatus.Hidden,
                 Duration = 15,
-                BuyNowEnabled = true
+                BuyNowEnabled = false
             };
 
             var products = await productService.GetProducts("en");
@@ -53,10 +50,7 @@ namespace Mzayad.Web.Areas.admin.Models.Auction
                 
             }).ToList();
 
-            
-
             return this;
-
         }
     }
 }
