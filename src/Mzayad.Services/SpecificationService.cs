736dc91e3f09a8812292d64c16a079f9ec1b1755
samer alkhatib <samer_mail_2006@yahoo.com>
2015-03-05 14:16:48 +0200

    MZ-26 Add Specification  Controller + Services. Add Specification Index + Add Feature.

---
+++ src/Mzayad.Services/SpecificationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mzayad.Data;
+using Mzayad.Models;
+
+namespace Mzayad.Services
+{
+    public class SpecificationService : ServiceBase
+    {
+        public SpecificationService(IDataContextFactory dataContextFactory) : base(dataContextFactory)
+        {
+        }
+
+        public async Task<IEnumerable<Specification>> GetAll()
+        {
+            using (var dc=DataContext())
+            {
+                return await dc.Specifications.ToArrayAsync();
+            }
+        }
+
+        public async Task<Specification> Add(Specification specification)
+        {
+            using (var dc=DataContext())
+            {
+                dc.Specifications.Add(specification);
+                await dc.SaveChangesAsync();
+                return specification;
+            }
+        }
+    }
+}
---
+++ src/Mzayad.Web/Areas/admin/Controllers/SpecificationsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Mzayad.Models;
+using Mzayad.Services;
+using Mzayad.Web.Controllers;
+using Mzayad.Web.Core.Services;
+using OrangeJetpack.Localization;
+
+namespace Mzayad.Web.Areas.admin.Controllers
+{
+    public class SpecificationsController : ApplicationController
+    {
+
+        private readonly SpecificationService _specificationService;
+
+        public SpecificationsController(IControllerServices controllerServices) : base(controllerServices)
+        {
+            _specificationService=new SpecificationService(DataContextFactory);
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var model = await _specificationService.GetAll();
+            return View(model);
+        }
+
+        public async Task<ActionResult> Add()
+        {
+            var model = new Specification()
+            {
+                Name = LocalizedContent.Init()
+            };
+            return View(model);
+        }
+
+
+        [HttpPost,ValidateAntiForgeryToken]
+        public async Task<ActionResult> Add(Specification model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var specification = await _specificationService.Add(model);
+            specification.Localize("en", i => i.Name);
+            SetStatusMessage(string.Format("Specification {0} has been added successfully.",specification.Name));
+            return RedirectToAction("Index");
+        }
+    }
+}
