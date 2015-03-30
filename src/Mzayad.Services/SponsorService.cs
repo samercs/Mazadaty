e75ce51e1f762f9b0e61e3b9487d99df0e799a69
samer alkhatib <samer_mail_2006@yahoo.com>
2015-03-30 14:52:41 +0300

    MZ-40 create a SponsorService and a SponsorsController

---
+++ src/Mzayad.Services/SponsorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mzayad.Data;
+
+namespace Mzayad.Services
+{
+    public class SponsorService :ServiceBase
+    {
+        public SponsorService(IDataContextFactory dataContextFactory) : base(dataContextFactory)
+        {
+        }
+    }
+}
---
+++ src/Mzayad.Web/Areas/admin/Controllers/SponsorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mzayad.Web.Areas.admin.Controllers
+{
+    public class SponsorsController : Controller
+    {
+        // GET: admin/Sponsors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
