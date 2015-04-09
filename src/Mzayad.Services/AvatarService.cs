2aea508e3d521a8b7f47e877fd4df994fe4e2dd5
samer alkhatib <samer_mail_2006@yahoo.com>
2015-04-09 14:27:59 +0300

    MZ-50 create AvatarService and ~/Areas/Admin/AvatarsController.

---
+++ src/Mzayad.Services/AvatarService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mzayad.Data;
+
+namespace Mzayad.Services
+{
+    public class AvatarService : ServiceBase
+    {
+        public AvatarService(IDataContextFactory dataContextFactory) : base(dataContextFactory)
+        {
+        }
+    }
+}
---
+++ src/Mzayad.Web/Areas/admin/Controllers/AvatarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mzayad.Web.Areas.admin.Controllers
+{
+    public class AvatarsController : Controller
+    {
+        // GET: admin/Avatar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
