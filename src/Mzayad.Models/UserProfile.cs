c5aa83b261fb4be2fc69f197b1e329ead5d49f8f
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-04-14 12:50:01 +0300

    MZ-52 removed UserProfile.Gamertag

--- src/Mzayad.Models/UserProfile.cs
+++ src/Mzayad.Models/UserProfile.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Mzayad.Models.Enum;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-using Mzayad.Models.Enum;
 
 namespace Mzayad.Models
 {
@@ -17,17 +11,19 @@ namespace Mzayad.Models
         public string UserId { get; set; }
         [Required]
         public UserProfileStatus Status { get; set; }
-        [Required, StringLength(20), Index("IX_Gamertag", IsUnique = true)]
-        public string Gamertag { get; set; }
+
         [Required, Index("IX_ProfileUrl", IsUnique = true),StringLength(255)]
         public string ProfileUrl { get; set; }
         public int? AvatarId { get; set; }
-        
-        
+                
         [ForeignKey("AvatarId")] 
         public virtual Avatar Avatar { get; set; }
         [ForeignKey("UserId")]
         public virtual ApplicationUser User { get; set; }
 
+        public static string GenerateProfileUrl(string username)
+        {
+            return string.Format("https://www.mzayad.com/profiles/{0}", username.ToLowerInvariant());
+        }
     }
 }
