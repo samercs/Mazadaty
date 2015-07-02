392a387904f4b831e9f49e4dad5124ce6d896ab0
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-02 17:06:02 +0300

    MZ-71 changed level table to display levels 1 - 100 and not 0 - 100

--- src/Mzayad.Web/Areas/admin/Controllers/LevelsController.cs
+++ src/Mzayad.Web/Areas/admin/Controllers/LevelsController.cs
@@ -1,21 +1,17 @@
 ﻿using Mzayad.Models;
 using Mzayad.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Mzayad.Web.Areas.Admin.Controllers
 {
     public class LevelsController : Controller
     {
-        // GET: Admin/Levels
         public ActionResult Index()
         {
             var levels = new List<Level>();
             var lvlService = new LevelService();
-            for (var x = 0; x < 101; x++)
+            for (var x = 1; x <= 100; x++)
             {
                 levels.Add(lvlService.GetLevel(x));
             }
