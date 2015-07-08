3079eed55a8bcf2fde8eb99521c8ac24fbe9209e
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-08 18:48:26 +0300

    MZ-76 added subscription price and sort order

--- src/Mzayad.Models/Subscription.cs
+++ src/Mzayad.Models/Subscription.cs
@@ -13,6 +13,11 @@ namespace Mzayad.Models
         public string Name { get; set; }
         public SubscriptionStatus Status { get; set; }
         public DateTime? ExpirationUtc { get; set; }
-        public int? Quantity { get; set; }     
+        public int? Quantity { get; set; }
+
+        public decimal? PurchasePrice { get; set; }
+        public int? PurchaseTokens { get; set; }
+
+        public double SortOrder { get; set; } 
     }
 }
