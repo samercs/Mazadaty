6624ffa8f6f43a2afe8a6c50ead4d86db322e2b2
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-09 15:44:58 +0300

    MZ-76 renamed subscription price properties

---
+++ src/Mzayad.Data/Migrations/201507091144131_RenameSubscriptionPriceFields.cs
@@ -0,0 +1,24 @@
+namespace Mzayad.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameSubscriptionPriceFields : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Subscriptions", "PriceCurrency", c => c.Decimal(precision: 18, scale: 3));
+            AddColumn("dbo.Subscriptions", "PriceTokens", c => c.Int());
+            DropColumn("dbo.Subscriptions", "PurchasePrice");
+            DropColumn("dbo.Subscriptions", "PurchaseTokens");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Subscriptions", "PurchaseTokens", c => c.Int());
+            AddColumn("dbo.Subscriptions", "PurchasePrice", c => c.Decimal(precision: 18, scale: 3));
+            DropColumn("dbo.Subscriptions", "PriceTokens");
+            DropColumn("dbo.Subscriptions", "PriceCurrency");
+        }
+    }
+}
--- src/Mzayad.Models/Subscription.cs
+++ src/Mzayad.Models/Subscription.cs
@@ -17,16 +17,26 @@ namespace Mzayad.Models
         public DateTime? ExpirationUtc { get; set; }
         public int? Quantity { get; set; }
 
-        public decimal? PurchasePrice { get; set; }
-        public int? PurchaseTokens { get; set; }
+        public decimal? PriceCurrency { get; set; }
+        public int? PriceTokens { get; set; }
 
         public double SortOrder { get; set; }
-        
+
+        public bool PriceCurrencyIsValid
+        {
+            get { return PriceCurrency.HasValue && PriceCurrency.Value > 0; }
+        }
+
+        public bool PriceTokensIsValid
+        {
+            get { return PriceTokens.HasValue && PriceTokens.Value > 0; }
+        }
+
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (PurchasePrice.GetValueOrDefault(0) <= 0 && PurchaseTokens.GetValueOrDefault(0) <= 0)
+            if (!PriceCurrencyIsValid || !PriceTokensIsValid)
             {
-                yield return new ValidationResult("Purchase price cannot be null or zero.", new[] { "PurchasePrice", "PurchaseTokens" });
+                yield return new ValidationResult("Purchase price cannot be null or zero.", new[] { "PriceCurrency", "PriceTokens" });
             }
         }
     }
