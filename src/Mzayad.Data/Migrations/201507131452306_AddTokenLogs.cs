71a465be2ed178a3e9696e2d88f71934d01b8a91
Andy Mehalick <mehalick@gmail.com>
2015-07-13 16:56:49 +0200

    MZ-76 added TokenLog db migration

---
+++ src/Mzayad.Data/Migrations/201507131452306_AddTokenLogs.cs
@@ -0,0 +1,39 @@
+namespace Mzayad.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTokenLogs : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TokenLogs",
+                c => new
+                    {
+                        TokenLogId = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ModifiedByUserId = c.String(nullable: false, maxLength: 128),
+                        OriginalTokenAmount = c.Int(),
+                        ModifiedTokenAmount = c.Int(nullable: false),
+                        UserHostAddress = c.String(nullable: false, maxLength: 15),
+                        CreatedUtc = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.TokenLogId)
+                .ForeignKey("dbo.AspNetUsers", t => t.ModifiedByUserId, cascadeDelete: false)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: false)
+                .Index(t => t.UserId)
+                .Index(t => t.ModifiedByUserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TokenLogs", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.TokenLogs", "ModifiedByUserId", "dbo.AspNetUsers");
+            DropIndex("dbo.TokenLogs", new[] { "ModifiedByUserId" });
+            DropIndex("dbo.TokenLogs", new[] { "UserId" });
+            DropTable("dbo.TokenLogs");
+        }
+    }
+}
