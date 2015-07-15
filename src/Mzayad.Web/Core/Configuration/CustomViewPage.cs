ce3260bf2310410a794a54621be94113254e68e6
Andy Mehalick <mehalick@gmail.com>
2015-07-15 11:26:21 +0200

    MZ-77 added CustomViewPage and F() wrapper method around StringFormatter.ObjectFormatter()

---
+++ src/Mzayad.Web/Core/Configuration/CustomViewPage.cs
@@ -0,0 +1,57 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using Mzayad.Web.Core.Services;
+using OrangeJetpack.Base.Core.Formatting;
+
+namespace Mzayad.Web.Core.Configuration
+{
+    public abstract class CustomViewPage : WebViewPage
+    {
+        private IAppSettings _appSettings;
+        public IAppSettings AppSettings
+        {
+            get
+            {
+                return (_appSettings = _appSettings ?? new AppSettings(ConfigurationManager.AppSettings));
+            }
+        }
+
+        public string LanguageCode
+        {
+            get { return ViewBag.LanguageCode ?? "en"; }
+        }
+
+        /// <summary>
+        /// Gets a formatting string from OrangeJetpack.Base.Core.Formatting.StringFormatter.ObjectFormat();
+        /// </summary>
+        public MvcHtmlString F(string format, object source)
+        {
+            return MvcHtmlString.Create(StringFormatter.ObjectFormat(format, source));
+        }
+    }
+
+    public abstract class CustomViewPage<TModel> : WebViewPage<TModel>
+    {
+        private IAppSettings _appSettings;
+        public IAppSettings AppSettings
+        {
+            get
+            {
+                return (_appSettings = _appSettings ?? new AppSettings(ConfigurationManager.AppSettings));
+            }
+        }
+
+        public string LanguageCode
+        {
+            get { return ViewBag.LanguageCode ?? "en"; }
+        }
+
+        /// <summary>
+        /// Gets a formatting string from OrangeJetpack.Base.Core.Formatting.StringFormatter.ObjectFormat();
+        /// </summary>
+        public MvcHtmlString F(string format, object source)
+        {
+            return MvcHtmlString.Create(StringFormatter.ObjectFormat(format, source));
+        }
+    }
+}
