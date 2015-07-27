28edebc44094baadfe6bd02b67d9e76bdd6965f2
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-27 17:02:16 +0300

    MZ-87 added ActivityEvent and ActivityService; added WindowsAzure.Storage NuGet package to Services project

---
+++ src/Mzayad.Models/ActivityEvent.cs
@@ -0,0 +1,10 @@
+﻿using Mzayad.Models.Enums;
+
+namespace Mzayad.Models
+{
+    public class ActivityEvent
+    {
+        public ActivityType Type { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ src/Mzayad.Services/Activity/ActivityService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Storage;
+
+namespace Mzayad.Services.Activity
+{
+    public interface IActivityService
+    {
+        
+    }
+
+    public class ActivityService : IActivityService
+    {
+        public ActivityService(string connectionString)
+        {
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+        }
+    }
+}
