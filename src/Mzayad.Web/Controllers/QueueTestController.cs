331fe7bd593fb10d4fa00980f412ef728cba1b92
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-27 17:02:36 +0300

    MZ-87 added QueueTestController

---
+++ src/Mzayad.Web/Controllers/QueueTestController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Mzayad.Web.Controllers
+{
+    [Authorize]
+    [RoutePrefix("queue")]
+    public class QueueTestController : Controller
+    {
+        [Route("test")]
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        [Route("test")]
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Test(FormCollection formCollection)
+        {
+            
+
+            return View();
+        }
+    }
+}
