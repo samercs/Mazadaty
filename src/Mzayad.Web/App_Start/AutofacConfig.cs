6d9fc7b2ccb7b7fb7041fb7173bdaa277c97aafe
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-27 17:05:35 +0300

    MZ-87 added IAppSettings.StorageConnection; added IActivityService registration to Autofac

--- src/Mzayad.Web/App_Start/AutofacConfig.cs
+++ src/Mzayad.Web/App_Start/AutofacConfig.cs
@@ -8,6 +8,7 @@ using Autofac.Integration.WebApi;
 using Autofac.Integration.SignalR;
 using Microsoft.AspNet.SignalR;
 using Mzayad.Data;
+using Mzayad.Services.Activity;
 using Mzayad.Web.Core.Services;
 using OrangeJetpack.Services.Client.Messaging;
 using OrangeJetpack.Services.Client.Storage;
@@ -40,6 +41,7 @@ namespace Mzayad.Web
             });
             
             builder.Register<IMessageService>(c => new MessageService(c.Resolve<IAppSettings>().EmailSettings));
+            builder.Register<IActivityService>(c => new ActivityService(c.Resolve<IAppSettings>().StorageConnection));
 
             builder.Register(GetCacheService).SingleInstance();
 
