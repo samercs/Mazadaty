4b56750ab14c134e0c5297f3747d2055c8210e13
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-07-27 17:00:45 +0300

    MZ-87 renamed "enum" folder to "enums"; added ActivityType

---
+++ src/Mzayad.Models/Enums/ActivityType.cs
@@ -0,0 +1,14 @@
+﻿namespace Mzayad.Models.Enums
+{
+    public enum ActivityType
+    {
+        TestActivity,
+        SubmitBid,
+        AutoBid,
+        WinAuction,
+        VerifyProfile,
+        ReferFriend,
+        VisitSite,
+        PurchaseSubscription
+    }
+}
