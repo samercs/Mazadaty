fdcddd086abfe745392e90fcfcdcba1be324cfd9
Andy Mehalick <andy.mehalick@orangejetpack.com>
2015-08-17 18:12:45 +0300

    MZ-93 fixed migration

--- src/Mzayad.Data/Migrations/201508171441380_DropActivityLogs.cs
+++ src/Mzayad.Data/Migrations/201508171441380_DropActivityLogs.cs
@@ -7,24 +7,24 @@ namespace Mzayad.Data.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.ActivityEvents",
-                c => new
-                    {
-                        ActivityEventId = c.Int(nullable: false, identity: true),
-                        Type = c.Int(nullable: false),
-                        UserId = c.String(),
-                        CreatedUtc = c.DateTime(nullable: false),
-                    })
-                .PrimaryKey(t => t.ActivityEventId);
+            //CreateTable(
+            //    "dbo.ActivityEvents",
+            //    c => new
+            //        {
+            //            ActivityEventId = c.Int(nullable: false, identity: true),
+            //            Type = c.Int(nullable: false),
+            //            UserId = c.String(),
+            //            CreatedUtc = c.DateTime(nullable: false),
+            //        })
+            //    .PrimaryKey(t => t.ActivityEventId);
             
-            DropColumn("dbo.OrderLogs", "UserHostAddress");
+            //DropColumn("dbo.OrderLogs", "UserHostAddress");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.OrderLogs", "UserHostAddress", c => c.String(nullable: false, maxLength: 15));
-            DropTable("dbo.ActivityEvents");
+            //AddColumn("dbo.OrderLogs", "UserHostAddress", c => c.String(nullable: false, maxLength: 15));
+            //DropTable("dbo.ActivityEvents");
         }
     }
 }
