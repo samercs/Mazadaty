57dc38741a8eb3c658f51d9ae9dec898bfe62e73
Amir Ismail <eng.amir.ismail@gmail.com>
2015-08-29 15:35:14 +0300

    MZ-98 refactoring Trophies Engine

---
+++ src/Mzayad.Services/Trophies/SubmitBidTrophyEngine.cs
@@ -0,0 +1,43 @@
+﻿using Mzayad.Data;
+using Mzayad.Models;
+using Mzayad.Models.Enum;
+using Mzayad.Services.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mzayad.Services.Trophies
+{
+    public class SubmitBidTrophyEngine : ITrophyEngine
+    {
+        private readonly UserService _userService;
+        private readonly TrophyService _trophyService;
+
+        public SubmitBidTrophyEngine(IDataContextFactory dataContextFactory)
+        {
+            _userService = new UserService(dataContextFactory);
+            _trophyService = new TrophyService(dataContextFactory);
+        }
+
+        public IEnumerable<Trophy> EvaluateActivityForPossibleTrophy(ApplicationUser user)
+        {
+            yield return CheckBidOnNewYears(user.Id);
+            // bid on Eid
+            // bid on birthday
+        }
+
+        private Trophy CheckBidOnNewYears(string userId)
+        {
+            if (DateTime.Now.Month != 1 || DateTime.Now.Day != 1)
+            {
+                return null;
+            }
+            var userTrophy = _trophyService.GetLastEarnedTrophy(TrophyKey.BidOnNewYear, userId).Result;
+            if (userTrophy.CreatedUtc.Year == DateTime.Now.Year)
+            {
+                return null;
+            }
+            return new Trophy() { TrophyId = (int)TrophyKey.BidOnNewYear };
+        }
+    }
+}
