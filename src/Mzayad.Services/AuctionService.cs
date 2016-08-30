c4cac664ca789a1c696d277416eb9a713b2569ba
Mohamed Badr <eng.ma.badr@gmail.com>
2016-08-30 13:43:02 +0300

    MZ-1674 filter deleted

--- src/Mzayad.Services/AuctionService.cs
+++ src/Mzayad.Services/AuctionService.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Mzayad.Services
@@ -44,6 +43,7 @@ namespace Mzayad.Services
             {
                 var auctions = await dc.Auctions
                     .Where(i => i.Status != AuctionStatus.Hidden)
+                    .Where(i => i.IsDeleted == false)
                     .Include(i => i.Product.ProductImages)
                     .Include(i => i.Product.ProductSpecifications.Select(j => j.Specification))
                     .Include(i => i.WonByUser)
@@ -111,6 +111,7 @@ namespace Mzayad.Services
         {
             return dc.Auctions
                 .Where(i => i.Status == auctionStatus)
+                .Where(i => i.IsDeleted == false)
                 .Include(i => i.Product.ProductImages)
                 //.Include(i => i.Product.ProductSpecifications.Select(j => j.Specification))
                 .Include(i => i.WonByUser)
@@ -150,13 +151,20 @@ namespace Mzayad.Services
         {
             using (var dc = DataContext())
             {
+                var auctions = dc.Auctions
+                    .Include(i => i.Product)
+                    .Where(i => i.IsDeleted == false);
+
                 if (!string.IsNullOrEmpty(search))
                 {
-                    return await dc.Auctions.Include(i => i.Product).Where(i => i.Product.Name.Contains(search)).OrderByDescending(i => i.StartUtc).ToListAsync();
 
+                    auctions = auctions
+                        .Where(i => i.Product.Name.Contains(search));
                 }
 
-                return await dc.Auctions.Include(i => i.Product).OrderByDescending(i => i.StartUtc).ToListAsync();
+                return await auctions
+                    .OrderByDescending(i => i.StartUtc)
+                    .ToListAsync();
             }
         }
 
@@ -166,6 +174,7 @@ namespace Mzayad.Services
             {
                 var auctions = await dc.Auctions
                     .Where(i => i.WonByUserId == userId)
+                    .Where(i => i.IsDeleted == false)
                     .Include(i => i.Product.ProductImages)
                     .OrderByDescending(i => i.ClosedUtc)
                     .ToListAsync();
@@ -202,6 +211,7 @@ namespace Mzayad.Services
         private static async Task<Auction> GetAuction(IDataContext dc, int auctionId)
         {
             return await dc.Auctions
+                .Where(i => i.IsDeleted == false)
                 .Include(i => i.Product.Categories)
                 .Include(i => i.Product.ProductImages)
                 .Include(i => i.Product.ProductSpecifications.Select(j => j.Specification))
@@ -261,6 +271,7 @@ namespace Mzayad.Services
             using (var dc = DataContext())
             {
                 return await dc.Auctions
+                    .Where(i => i.IsDeleted == false)
                     .CountAsync(i => i.WonByUserId == userId && (
                                                                 (from.HasValue && i.CreatedUtc >= from.Value)
                                                                 ||
@@ -276,6 +287,7 @@ namespace Mzayad.Services
             using (var dc = DataContext())
             {
                 var wonAuctionsDates = await dc.Auctions
+                    .Where(i => i.IsDeleted == false)
                     .Where(i => i.WonByUserId == userId && i.ClosedUtc.HasValue)
                     .OrderByDescending(i => i.ClosedUtc)
                     .Select(i => i.ClosedUtc.Value)
