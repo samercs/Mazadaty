64f8ac04d6eaaa4d83ae45b423e721641329d7e4
Andy Mehalick <andy.mehalick@orangejetpack.com>
2017-03-05 14:23:30 +0300

    MZ-269 adding TrophyFunctions

---
+++ src/Mzayad.WebJobs/TrophyFunctions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs;
+using Mzayad.Models.Queues;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Mzayad.WebJobs
+{
+    public class TrophyFunctions
+    {
+        public async Task ProcessQueueItem([QueueTrigger("%trophies%")] UserTrophyMessage message, TextWriter log)
+        {
+            await log.WriteLineAsync($"Handling messing: {message.ToJson()}...");
+
+            
+        }
+    }
+}
