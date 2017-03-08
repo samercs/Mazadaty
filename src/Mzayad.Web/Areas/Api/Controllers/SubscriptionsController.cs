f1e9dd0df32703802932ddb577465b9c7fe2955e
samer <samer_mail_2006@yahoo.com>
2017-03-08 11:37:54 +0300

    MZ-276 add subscription list API.

---
+++ src/Mzayad.Web/Areas/Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,29 @@
+﻿using Mzayad.Services;
+using Mzayad.Web.Areas.Api.Models.Subscriptions;
+using Mzayad.Web.Core.Services;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Mzayad.Web.Areas.Api.Controllers
+{
+    [RoutePrefix("api/subscriptions")]
+    public class SubscriptionsController : ApplicationApiController
+    {
+        private readonly SubscriptionService _subscriptionService;
+        public SubscriptionsController(IAppServices appServices) : base(appServices)
+        {
+            _subscriptionService = new SubscriptionService(DataContextFactory);
+        }
+
+
+        [Route("")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            var subscriptions = await _subscriptionService.GetActiveSubscriptions(Language);
+            return Ok(subscriptions.Select(SubscriptionViewModel.Create));
+        }
+
+    }
+}
---
+++ src/Mzayad.Web/Areas/Api/Models/Subscriptions/SubscriptionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Mzayad.Models;
+
+namespace Mzayad.Web.Areas.Api.Models.Subscriptions
+{
+    public class SubscriptionViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Token { get; set; }
+        public decimal? Prices { get; set; }
+        public int Duration { get; set; }
+
+        public static SubscriptionViewModel Create(Subscription subscription)
+        {
+            return new SubscriptionViewModel
+            {
+                Id = subscription.SubscriptionId,
+                Token = subscription.PriceTokens,
+                Name = subscription.Name,
+                Prices = subscription.PriceCurrency,
+                Duration = subscription.Duration
+            };
+        }
+    }
+}
