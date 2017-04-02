5666aa81e6293566d969685e800fa4b3da1c7f12
samer <samer_mail_2006@yahoo.com>
2017-04-02 10:57:18 +0300

    MZ-282 fix user name with dot profiles.

--- src/Mzayad.Web/Controllers/UserController.cs
+++ src/Mzayad.Web/Controllers/UserController.cs
@@ -431,7 +431,7 @@ namespace Mzayad.Web.Controllers
             return RedirectToAction("Dashboard");
 
         }
-        [Route("~/{language}/profiles/{userName}")]
+        //[Route("~/{language}/profiles/{userName:regex(^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$)}")]
         public async Task<ActionResult> UserProfile(string userName)
         {
             var user = await _userService.GetUserByName(userName);
