8da5988c59fe27a2b80d7a4405771f24bd669b0a
samer <samer_mail_2006@yahoo.com>
2017-04-16 16:23:50 +0300

    MZ-328 add ads api.

---
+++ src/Mzayad.Web/Areas/Api/Models/Ads/AdsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mzayad.Web.Areas.Api.Models.Ads
+{
+    public class AdsViewModel
+    {
+        public string ImageUrl { get; set; }
+        public int MobileAdId { get; set; }
+        public int Weight { get; set; }
+    }
+}
