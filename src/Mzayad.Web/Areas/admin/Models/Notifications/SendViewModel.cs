c26e56c9a7543e3e57bc265e2af3588aff081b11
Andy Mehalick <andy.mehalick@orangejetpack.com>
2017-05-20 15:50:55 +0300

    MZ-343 adding push notifications

---
+++ src/Mzayad.Web/Areas/admin/Models/Notifications/SendViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mzayad.Web.Areas.Admin.Models.Notifications
+{
+    public class SendViewModel
+    {
+        [Required]
+        public string Topic { get; set; } = "/topics/all";
+
+        [Required, StringLength(40)]
+        public string Title { get; set; }
+
+        [Required, StringLength(140)]
+        public string Message { get; set; }
+    }
+}
